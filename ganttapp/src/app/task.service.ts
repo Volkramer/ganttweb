f0fdf2290b8ae29798a29fec212d6aafa5053a4b
Frederic Fleisch <volkramer@gmail.com>
2022-05-13 14:12:54 +0200

    update task

--- ganttapp/src/app/task.service.ts
+++ ganttapp/src/app/task.service.ts
@@ -1,16 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Task } from './task';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
-  public getTasks(): Observable<any> {
-    return this.http.get<any>(`${this.apiServerUrl}/task/all`);
+  public getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(`${this.apiServerUrl}/task/all`);
+  }
+
+  public getTaskById(taskId: number): Observable<Task> {
+    return this.http.get<Task>(`${this.apiServerUrl}/task/find/${taskId}`);
+  }
+
+  /* public getTaskById(taskId: number): Observable<Task> {
+    return this.http.get<Task>(this.apiServerUrl + '/task/find/' + taskId);
+  } */
+
+  public addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(`${this.apiServerUrl}/task/add`, task);
+  }
+
+  public updateTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this.apiServerUrl}/task/update`, task);
+  }
+
+  public deleteTask(taskId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/task/delete/${taskId}`);
   }
 }
